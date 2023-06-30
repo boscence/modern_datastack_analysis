383b918ccffc160643ffa3dbd89f058f7daa2a0c
Andrew Boscence <boscence@gmail.com>
2023-06-30 11:57:04 +0800

    fixes #2 dump html to json in local

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
-from src import get_stacks_url 
+from src import get_stacks_url
+from src import get_data 
 import config
 
 
 html_data = get_stacks_url.get_stack_html_data(config.URL_GET_STACKS)
 stack_urls = get_stacks_url.get_stack_urls(html_data, config.URL_STACK_BASE)
 
-
-for url in stack_urls:
-    print(url)
+for stacks in stack_urls:
+    json_data = get_data.get_json(stacks)
+    get_data.get_json_file(json_data,stacks)
