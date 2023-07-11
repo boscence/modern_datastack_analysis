df7f70abdf64cdb57d4d433b8eb9d4cd04cf75f8
Andrew Boscence <boscence@gmail.com>
2023-07-12 00:09:58 +0800

    fixes #8 renamed folders and write paths

--- run.py
+++ run.py
@@ -3,22 +3,26 @@ from src import get_data
 import os
 import config
 
-
+### Set the constants
+# get the html data containing all stack urls.
 html_data = get_stacks_url.get_stack_html_data(config.URL_GET_STACKS)
+# now get the urls
 stack_urls = get_stacks_url.get_stack_urls(html_data, config.URL_STACK_BASE)
 
-
+# for each url
 for stack in stack_urls:
-    json_data = get_data.get_json(stack)
-    get_data.get_json_file(json_data,stack)
+    json_data = get_data.get_json(stack) # get the data
+    get_data.get_json_file(json_data,stack) # write the json
     print(f'Written:{stack}')
 
+# get the list of json files
+json_files = [json_file for json_file in os.listdir("data/source/mds") if json_file.endswith('.json')]
 
-json_files = [json_file for json_file in os.listdir("data") if json_file.endswith('.json')]
-
-
+# for each json file (stack)
 for json_file in json_files:
-    data = get_data.load_json(json_file)
-    company_data = get_data.get_company_table(data)
-    stack_data = get_data.get_stack_table(data, company_data)
-    get_data.write_parquet(stack_data,json_file)
+    print(f"Writing {json_file}")
+    data = get_data.load_json(json_file) # load it
+    company_data = get_data.get_company_table(data) # get the data about the company
+    stack_data = get_data.get_stack_table(data, company_data)  # get the stack data
+    get_data.write_stack_parquet(stack_data,json_file) # write stack data to parquet
+    get_data.write_company_parquet(company_data,json_file) # write company data to parquet
